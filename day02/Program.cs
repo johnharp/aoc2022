4266612b0e22fc9471398fcac6c3be3286d11a2f
John Harper <john@harper.link>
2022-12-01 08:48:40 -0600

    empty project for day02

---
+++ day02/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+namespace day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] lines = File.ReadAllLines("../../../input.txt");
+
+            Console.WriteLine($"total lines: {lines.Length}");
+        }
+    }
+}
