cd8238cca830ba630a2eb68a66ca82541cb934b6
John Harper <john@harper.link>
2022-12-03 08:41:44 -0600

    day03 part 1

--- day02/Program.cs
+++ day02/Program.cs
@@ -8,8 +8,135 @@ namespace day02
         static void Main(string[] args)
         {
             String[] lines = File.ReadAllLines("../../../input.txt");
+            Solve(lines);
+        }
 
-            Console.WriteLine($"total lines: {lines.Length}");
+        static void Solve(String[] lines)
+        {
+            int totalScorePartA = 0;
+            int totalScorePartB = 0;
+
+            foreach(var l in lines)
+            {
+                int scoreA = scoreLine(l, isPartTwo: false);
+                int scoreB = scoreLine(l, isPartTwo: true);
+                totalScorePartA += scoreA;
+                totalScorePartB += scoreB;
+            }
+
+            Console.WriteLine("Part A:");
+            Console.WriteLine(totalScorePartA);
+            Console.WriteLine("-----------------");
+            Console.WriteLine("Part B:");
+            Console.WriteLine(totalScorePartB);
+
+        }
+
+
+
+        static int scoreLine(string l, bool isPartTwo)
+        {
+            (int, int) choices = decodeLine(l);
+
+            if (isPartTwo)
+            {
+                // in part two, the second item in the line is not
+                // which hand to play, it instead means:
+                // X or 1 == you should lose
+                // Y or 2 == you should draw
+                // Z or 3 == you should win
+                // convert this back into what to play
+
+                // Should lose
+                if (choices.Item2 == 1)
+                {
+                    switch(choices.Item1)
+                    {
+                        case 1: // if rock, use scissors
+                            choices.Item2 = 3;
+                            break;
+                        case 2: // if paper, use rock
+                            choices.Item2 = 1;
+                            break;
+                        case 3: // if scissors, use paper
+                            choices.Item2 = 2;
+                            break;
+                    }
+                }
+
+                // Should draw
+                else if (choices.Item2 == 2)
+                {
+                    choices.Item2 = choices.Item1;
+                }
+
+                // Should win
+                else if (choices.Item2 == 3)
+                {
+                    switch (choices.Item1)
+                    {
+                        case 1: // if rock, use paper
+                            choices.Item2 = 2;
+                            break;
+                        case 2: // if paper, use scissors
+                            choices.Item2 = 3;
+                            break;
+                        case 3: // if scissors, use rock
+                            choices.Item2 = 1;
+                            break;
+                    }
+                }
+
+
+            }
+
+
+            int score = 0;
+
+            // add 1 for rock, 2 for paper, 3 for scissors
+            score += choices.Item2;
+
+            // if it's a draw get 3 more points
+            if (choices.Item2 == choices.Item1) score += 3;
+
+            // check for a win (that's 6 more points)
+            // rock/scissors
+            // paper/rock
+            // scissors/paper
+            if ((choices.Item2 == 1 && choices.Item1 == 3) ||
+                (choices.Item2 == 2 && choices.Item1 == 1) ||
+                (choices.Item2 == 3 && choices.Item1 == 2)) score += 6;
+
+            return score;
+        }
+
+        static (int, int) decodeLine(string l)
+        {
+            String[] parts = l.Split(' ');
+
+            return (decode(parts[0]), decode(parts[1]));
+        }
+
+        static int decode(string item)
+        {
+            switch(item)
+            {
+                case "A":
+                    return 1;
+                case "B":
+                    return 2;
+                case "C":
+                    return 3;
+                case "X":
+                    return 1;
+                case "Y":
+                    return 2;
+                case "Z":
+                    return 3;
+                default:
+                    throw new Exception("bad input!");
+
+            }
         }
     }
 }
---
+++ day03/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../input.txt");
+            Solve(lines);
+        }
+
+        static void Solve(String[] lines)
+        {
+            long totalOfPriorities = 0;
+
+            foreach(var line in lines)
+            {
+                totalOfPriorities += SolveLine(line);
+            }
+
+            Console.WriteLine($"Total priority of matching items: {totalOfPriorities}");
+        }
+
+        static long SolveLine(String line)
+        {
+            long priorityOfMatchingItems = 0;
+
+            int numItems = line.Length;
+            int itemsPerCompartment = numItems / 2;
+
+            String leftItems = line.Substring(0, itemsPerCompartment);
+            String rightItems = line.Substring(itemsPerCompartment, itemsPerCompartment);
+
+            char match = FindMatchingItem(leftItems, rightItems);
+            long priority = priorityOfMatchingItem(match);
+
+            priorityOfMatchingItems += priority;
+
+            return priorityOfMatchingItems;
+        }
+
+        static char FindMatchingItem(String left, String right)
+        {
+            HashSet<char> leftHash = new HashSet<char>(left);
+            foreach(char c in right.ToCharArray())
+            {
+                if (leftHash.Contains(c)) return c;
+            }
+
+            return ' ';
+        }
+
+        static long priorityOfMatchingItem(char c)
+        {
+            long value = 0;
+
+            if (Char.IsUpper(c))
+            {
+                value = c - 'A' + 27;
+            }
+            else
+            {
+                value = c - 'a' + 1;
+            }
+
+            return value;
+        }
+    }
+}
