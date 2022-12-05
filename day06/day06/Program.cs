61703d869858636ea5ed700bdc28c805929da166
John Harper <john@harper.link>
2022-12-05 16:09:41 -0600

    empty project for day06

---
+++ day06/day06/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+string[] lines = File.ReadAllLines("../../../input-example.txt");
+
+parseInput();
+solve();
+
+void parseInput()
+{
+    foreach(var line in lines)
+    {
+
+    }
+}
+
+void solve()
+{
+
+}
