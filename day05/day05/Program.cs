59d131b3e96bf73e4eb4afd7c09bac501c66a176
John Harper <john@harper.link>
2022-12-04 20:38:28 -0600

    blank project for day05

---
+++ day05/day05/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace day05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] lines = File.ReadAllLines("../../../input-example.txt");
+
+            Console.WriteLine($"Number of lines of input: {lines.Length}");
+            
+        }
+    }
+}
