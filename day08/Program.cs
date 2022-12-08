e00de7438b3db650cdec9762499eda2bc103dfb2
John Harper <john@harper.link>
2022-12-08 09:19:08 -0600

    day08 part1 complete

--- day08/Program.cs
+++ day08/Program.cs
@@ -3,7 +3,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        var lines = File.ReadAllLines("../../../input-example.txt");
+        var lines = File.ReadAllLines("../../../input.txt");
+        TreeMap m = new TreeMap(lines);
+
+        //Console.WriteLine($"# Cols: {m.NumCols}");
+        //Console.WriteLine($"# Rows: {m.NumRows}");
+
+        int numberTreesVisible = m.CalculateVisMap();
+
+        Console.WriteLine("Map:");
+        m.PrintMap();
+
+        Console.WriteLine();
+        Console.WriteLine("VisMap ");
+        m.PrintVisMap();
+
+        Console.WriteLine();
+        Console.WriteLine($"Total Trees Visible: {numberTreesVisible}");
+
     }
 }
 
