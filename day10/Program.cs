a351c4f6a990bf1e54b11f957f8eb5e5e1fc8a3d
John Harper <john@harper.link>
2022-12-10 00:10:22 -0600

    start of day 10 -- part 1 done

---
+++ day10/Program.cs
@@ -0,0 +1,48 @@
+﻿string filename;
+
+filename = "../../../input.txt";
+//filename = "../../../input-example.txt";
+string[] lines = File.ReadAllLines(filename);
+
+// xvalues contains the value of the X register
+// throughout the given clock cycle
+List<long> xvalues = new List<long>();
+// initial value of X is 1
+xvalues.Add(1); // cycle 0
+
+long nextx = 1;
+
+// start of cycle 2
+foreach (var line in lines)
+{
+    string[] parts = line.Split();
+    if (parts[0] == "noop")
+    {
+        xvalues.Add(nextx);
+        // nextx retains its value
+    }
+    else if (parts[0] == "addx")
+    {
+        long v = long.Parse(parts[1]);
+        xvalues.Add(nextx);
+        xvalues.Add(xvalues.Last());
+        nextx = v + xvalues.Last();
+    }
+}
+
+xvalues.Add(nextx);
+
+//for(int clock=0; clock<xvalues.Count; clock++)
+//{
+//    Console.WriteLine($"clock {clock}: x = {xvalues[clock]}");
+//}
+
+long totalStrength = 0;
+for (int i = 20; i < xvalues.Count; i += 40)
+{
+    long strength = i * xvalues[i];
+    totalStrength += strength;
+}
+
+Console.WriteLine($"Total signal strength = {totalStrength}");
+
