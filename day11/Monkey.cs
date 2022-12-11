bd42c64bdafe8f28dff17402abe5e3b7108c1c94
John Harper <john@harper.link>
2022-12-11 08:09:33 -0600

    ready to start part 2 refactor day 11

---
+++ day11/Monkey.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Numerics;
+
+namespace day11
+{
+	public class Monkey
+	{
+		public List<long> Items;
+		public Func<long, long> Operation;
+		public Func<long, int> Test;
+
+		public long NumItemsInspected = 0;
+
+		public static List<Monkey> All = new List<Monkey>();
+
+		public static void CompleteRound()
+		{
+			foreach(var monkey in All)
+			{
+				monkey.TakeTurn();
+			}
+		}
+
+		public static void PrintAll()
+		{
+			for(int i =0; i<All.Count; i++)
+			{
+				Monkey m = All[i];
+				Console.WriteLine($"Monkey {i}  [num inpected {m.NumItemsInspected}]: {m.ItemsToString()}");
+            }
+        }
+
+		public static void PrintMonkeyBusinessLevel()
+		{
+			List<long> numItems = new List<long>();
+			foreach(var m in Monkey.All)
+			{
+				numItems.Add(m.NumItemsInspected);
+			}
+			numItems.Sort();
+
+			long v = numItems[numItems.Count - 2] * numItems[numItems.Count - 1];
+			Console.WriteLine($"Total monkey-business level: {v}");
+		}
+
+		public string ItemsToString()
+		{
+			string s = string.Join(", ", Items.ToArray());
+			return s;
+		}
+
+        public Monkey()
+		{
+			Items = new List<long>();
+			Operation = (long old) => old;
+			Test = (long x) => 0;
+        }
+
+		public void TakeTurn()
+		{
+			if (Items == null || Items.Count == 0) return;
+
+			List<long> items = Items;
+			Items = new List<long>();
+
+			foreach (var item in items)
+			{
+				long value = Operation(item);
+				//value = value / 3;
+				value = Reduce(value);
+				int targetMonkey = Test(value);
+
+				All[targetMonkey].Items.Add(value);
+
+				NumItemsInspected++;
+            }
+		}
+
+		public long Reduce(long value)
+		{
+			long v = 1;
+
+			if (value % 19 == 0) { v *= 19; }
+			if (value % 17 == 0) { v *= 17; }
+			if (value % 13 == 0) { v *= 13; }
+			if (value % 11 == 0) { v *= 11; }
+			if (value % 7 == 0) { v *= 7; }
+			if (value % 5 == 0) { v *= 5; }
+            if (value % 3 == 0) { v *= 3; }
+            if (value % 2 == 0) { v *= 2; }
+
+            return v;
+		}
+	}
+}
+
--- day11/Program.cs
+++ day11/Program.cs
@@ -1,3 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using day11;
+using System.Numerics;
 
+//CreateExampleMonkeys();
+CreatePart1Monkeys();
+
+
+for (int i = 1; i <= 10000; i++)
+{
+    //Console.WriteLine($"After round {i}:");
+    Monkey.CompleteRound();
+    //Monkey.PrintAll();
+    //Console.WriteLine();
+}
+
+Monkey.PrintMonkeyBusinessLevel();
+
+
+
+void CreateExampleMonkeys()
+{
+    Monkey m;
+
+    m = new Monkey();
+    m.Items = new List<long> { 79, 98 };
+    m.Operation = (long old) => old * 19;
+    m.Test = (long x) => (x % 23 == 0) ? 2 : 3;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 54, 65, 75, 74 };
+    m.Operation = (long old) => old + 6;
+    m.Test = (long x) => (x % 19 == 0) ? 2 : 0;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 79, 60, 97 };
+    m.Operation = (long old) => old * old;
+    m.Test = (long x) => (x % 13 == 0) ? 1 : 3;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 74 };
+    m.Operation = (long old) => old + 3;
+    m.Test = (long x) => (x % 17 == 0) ? 0 : 1;
+    Monkey.All.Add(m);
+}
+
+void CreatePart1Monkeys()
+{
+    Monkey m;
+
+    m = new Monkey();
+    m.Items = new List<long> { 66, 79 };
+    m.Operation = (long old) => old * 11;
+    m.Test = (long x) => (x % 7 == 0) ? 6 : 7;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 84, 94, 94, 81, 98, 75 };
+    m.Operation = (long old) => old * 17;
+    m.Test = (long x) => (x % 13 == 0) ? 5 : 2;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 85, 79, 59, 64, 79, 95, 67 };
+    m.Operation = (long old) => old + 8;
+    m.Test = (long x) => (x % 5 == 0) ? 4 : 5;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 70 };
+    m.Operation = (long old) => old + 3;
+    m.Test = (long x) => (x % 19 == 0) ? 6 : 0;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 57, 69, 78, 78 };
+    m.Operation = (long old) => old + 4;
+    m.Test = (long x) => (x % 2 == 0) ? 0 : 3;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 65, 92, 60, 74, 72 };
+    m.Operation = (long old) => old + 7;
+    m.Test = (long x) => (x % 11 == 0) ? 3 : 4;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 77, 91, 91 };
+    m.Operation = (long old) => old * old;
+    m.Test = (long x) => (x % 17 == 0) ? 1 : 7;
+    Monkey.All.Add(m);
+
+    m = new Monkey();
+    m.Items = new List<long> { 76, 58, 57, 55, 67, 77, 54, 99 };
+    m.Operation = (long old) => old + 6;
+    m.Test = (long x) => (x % 3 == 0) ? 2 : 1;
+    Monkey.All.Add(m);
+}
