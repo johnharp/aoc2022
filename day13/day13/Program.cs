934cee9d309708298177dab194174e7d3bf22d5a
John Harper <john@harper.link>
2022-12-13 08:44:22 -0600

    start of parsing day 13

--- day13/day13/Program.cs
+++ day13/day13/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] lines = File.ReadAllLines("../../../input.txt");
+            List<(Term, Term)> Pairs = new List<(Term, Term)> ();
+
+            int i = 0;
+            while (i < lines.Length)
+            {
+                string leftstr = lines[i++];
+                string rightstr = lines[i++];
+
+                Pairs.Add((new Term(leftstr), new Term(rightstr)));
+
+                if (i < lines.Length-1) { i++; }
+            }
+
+            Console.WriteLine(Pairs.First());
+            Console.WriteLine(Pairs.Last());
         }
     }
 }
---
+++ day13/day13/Term.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day13
+{
+    public class Term
+    {
+        public String OriginalStringRep = "";
+
+        bool IsList = false;
+        public int Value = 0;
+        public List<Term> Terms = new List<Term>();
+
+        public Term(string s)
+        {
+            OriginalStringRep = s;
+            s = s.Trim();
+            if (s.StartsWith("["))
+            {
+                IsList = true;
+            }
+            else
+            {
+                IsList = false;
+                Value = int.Parse(s);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            if (IsList == true)
+            {
+                sb.Append("[");
+                foreach(Term t in Terms)
+                {
+                    sb.Append(t.ToString());
+                    if (t != Terms.Last())
+                    {
+                        sb.Append(",");
+                    }
+                }
+                sb.Append("]");
+            }
+            else
+            {
+                sb.Append(Value);
+            }
+            return sb.ToString();
+        }
+    }
+}
