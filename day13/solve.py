e388d87bbc91b5d80fe995c9de7e98fa402ea902
John Harper <john@harper.link>
2023-02-18 16:54:24 -0600

    finished day13 part2

--- day13/solve.py
+++ day13/solve.py
@@ -1,8 +1,9 @@
 from list import *
+from functools import cmp_to_key
 
+packets = []
 pairs = []
 indicies_of_right_ordered_pairs = []
-
 
 # Compare two packets given the rules outlined
 # in the problem description
@@ -11,16 +12,24 @@ indicies_of_right_ordered_pairs = []
 #  negative value if left < right
 #   0 if left == right
 #   positive value if left > right
-def comp(left, right, depth):
+def comp(left, right):
+
+    if left == right:
+        return 0
 
     if is_integer(left) and is_list(right):
         left = integer_to_list(left)
-        return comp(left, right, depth)
+        return comp(left, right)
     elif is_list(left) and is_integer(right):
         right = integer_to_list(right)
-        return comp(left, right, depth)
+        return comp(left, right)
     elif is_integer(left) and is_integer(right):
-        return int(left) - int(right)
+        if int(left) < int(right):
+            return -1
+        elif int(left) > int(right):
+            return 1
+        else:
+            return 0
     elif is_list(left) and is_list(right):
         left_values = list_values(left)
         right_values = list_values(right)
@@ -41,7 +50,7 @@ def comp(left, right, depth):
             left_value = left_values[i]
             right_value = right_values[i]
 
-            result = comp(left_value, right_value, depth+1)
+            result = comp(left_value, right_value)
             
             if result < 0:
                 return result
@@ -53,7 +62,7 @@ def comp(left, right, depth):
         # left side ran out of items so in right order
         return -1
 
-    return 0
+    return -1
 
 # Decides if a value is a list
 # 
@@ -78,24 +87,45 @@ def integer_to_list(v):
 
 
 with open('input.txt', 'r') as f:
-    while True:
-        left = f.readline()
-        if left == "": break
+    for line in f:
+        line = line.strip()
+        if line != "":
+            packets.append(line)
 
-        right = f.readline()
-        _ = f.readline() # throw away blank line between groups
-
-        pair = (left.strip(), right.strip())
-        pairs.append(pair)
+pair_num = 1
+total_pairs = len(packets) / 2
 
 
-for pair_num, pair in enumerate(pairs):
-    result = comp(pair[0], pair[1], 0)
+while pair_num <= total_pairs:
+    left_index = (pair_num-1) * 2
+    right_index = ((pair_num-1) * 2) + 1
 
-    if result <= 0:
-        indicies_of_right_ordered_pairs.append(pair_num + 1)
+    left = packets[left_index]
+    right = packets[right_index]
+
+    result = comp(packets[left_index], packets[right_index])
+    if result <=0 :
+        indicies_of_right_ordered_pairs.append(pair_num)
+    pair_num += 1
 
 
 
 print(f"Correct pair numbers: {indicies_of_right_ordered_pairs}")
 print(f"Sum: {sum(indicies_of_right_ordered_pairs)}")
+
+divider1 = "[[2]]"
+divider2 = "[[6]]"
+
+packets.append(divider1)
+packets.append(divider2)
+
+sorted_packets = sorted(packets, key=cmp_to_key(comp))
+
+divider1_index = sorted_packets.index(divider1) + 1
+divider2_index = sorted_packets.index(divider2) + 1
+
+product_divider_indicies = divider1_index * divider2_index
+
+print(f"divider 1 is at {divider1_index}")
+print(f"divider 2 is at {divider2_index}")
+print(f"Produce of marker indicies: {product_divider_indicies}")
