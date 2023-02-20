eb2741710679bff3a5eee2079dfa63fcd502f7a3
John Harper <john@harper.link>
2023-02-19 22:52:43 -0600

    day16 start

---
+++ day16/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace day16;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] lines = File.ReadAllLines("../../../input.txt");
+
+        foreach (var line in lines)
+        {
+            var v = new Valve(line);
+        }
+
+        foreach(var v in Valve.ValveDictionary.Values)
+        {
+            Console.WriteLine(v);
+        }
+    }
+}
+
