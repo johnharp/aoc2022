36dd8870fd91e5ca73903647b7f6e02816389e6b
John Harper <john@harper.link>
2023-02-28 15:04:30 -0600

    update

--- day16/Program.cs
+++ day16/Program.cs
@@ -5,14 +5,26 @@ class Program
     {
         string[] lines = File.ReadAllLines("../../../input-example.txt");
 
+        // Create all the valves
         foreach (var line in lines)
         {
             var v = new Valve(line);
         }
 
+        // Link the connected valves so each valve has a list of its neighbors
+        Valve.LinkAllConnectedValves();
+
+        // "Compact" all the valves so that the only zero-flow rate valve remaining is AA
+        foreach(var v in Valve.GetAllValves())
+        {
+
+        }
+
+
         foreach(var v in Valve.ValveDictionary.Values)
         {
             Console.WriteLine(v);
+            Console.WriteLine($"\tFully compacted? {v.IsFullyCompacted()}");
         }
     }
 }
--- day16/Valve.cs
+++ day16/Valve.cs
@@ -9,8 +9,15 @@ namespace day16
 		public long Rate { get; set; }
 
 		public List<String> ConnectedValveNames { get; set; }
+		public List<Valve> ConnectedValves { get; set; }
 
 		public static Dictionary<string, Valve> ValveDictionary = new Dictionary<string, Valve>();
+		public static List<Valve> GetAllValves()
+		{
+			var all = ValveDictionary.Values;
+
+			return all.ToList();
+		}
 
 		public Valve(string line)
 		{
@@ -33,13 +40,47 @@ namespace day16
 				ConnectedValveNames.Add(name);
 			}
 
+			ConnectedValves = new List<Valve>();
+
 			ValveDictionary[Name] = this;
         }
+
+		/// <summary>
+		/// Checks all the connected valves and verifies only "significant" valves are
+		/// connected.  (Those with a flow of 0 will never be opened so they just serve as connections
+		/// and should be traversed.)
+		/// </summary>
+		/// <returns>
+		///   True - all connected valves have a non-zero flow
+		///   False - one or more connected valves have a zero flow
+		/// </returns>
+		public bool IsFullyCompacted()
+		{
+			return !ConnectedValves.Any(x => x.Rate == 0);
+		}
+
+		public static void LinkAllConnectedValves()
+		{
+			foreach(var valve in GetAllValves())
+			{
+				valve.LinkConnectedValves();
+			}
+		}
+
+
+		private void LinkConnectedValves()
+		{
+			foreach(string valveName in ConnectedValveNames)
+			{
+				Valve v = ValveDictionary[valveName];
+				ConnectedValves.Add(v);
+			}
+		}
 
         public override string ToString()
         {
 			string connected =
-				string.Join(" | ", ConnectedValveNames);
+				string.Join(" | ", ConnectedValves.Select(x => x.Name));
             return $"{Name} rate={Rate} => {connected}";
         }
     }
